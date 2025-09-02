f0da649b7177289fbe550ace1db5eca169e8a5c0
SavvySantos <ssantos@byupathway.edu>
2025-09-02 15:53:38 -0600

    Program exercises for week 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,75 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string LetterGrade = "";
+
+        if (percentage >= 97)
+        {
+            LetterGrade = "A+";
+        }
+        else if (percentage <= 96 && percentage >= 94)
+        {
+            LetterGrade = "A";
+        }
+        else if (percentage <= 93 && percentage >= 90)
+        {
+            LetterGrade = "A-";
+        }
+        else if (percentage <= 89 && percentage >= 87)
+        {
+            LetterGrade = "B+";
+        }
+        else if (percentage <= 86 && percentage >= 84)
+        {
+            LetterGrade = "B";
+        }
+        else if (percentage <= 83 && percentage >= 80)
+        {
+            LetterGrade = "B-";
+        }
+        else if (percentage <= 79 && percentage >= 77)
+        {
+            LetterGrade = "C+";
+        }
+        else if (percentage <= 76 && percentage >= 74)
+        {
+            LetterGrade = "C";
+        }
+        else if (percentage <= 73 && percentage >= 70)
+        {
+            LetterGrade = "C-";
+        }
+        else if (percentage <= 69 && percentage >= 67)
+        {
+            LetterGrade = "D+";
+        }
+        else if (percentage <= 66 && percentage >= 64)
+        {
+            LetterGrade = "D";
+        }
+        else if (percentage <= 63 && percentage >= 60)
+        {
+            LetterGrade = "D-";
+        }
+        else
+        {
+            LetterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {LetterGrade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine($"You did not pass. Come back later to try again!");
+        }
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+
+        int squareNumber = SquareNumber(number);
+
+        DisplayResult(name, squareNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your full name? ");
+        string fullName = Console.ReadLine();
+
+        return fullName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        return favoriteNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square number is {square}.");
     }
 }
